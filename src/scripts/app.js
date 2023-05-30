4dd613a287188eee774bc91f79e349a15365dca0
Sandrine Wij <sandrinewij@gmail.com>
2023-05-30 20:58:46 +0200

    Drag chat bubbles and highlight on hover animation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,7 @@ import { gsap} from 'gsap';
 
 
 
-// Ouvrir div /////////////////////////////////////////////////////////////
+// Open div /////////////////////////////////////////////////////////////
 
 const pros = document.querySelectorAll('.pro');
 const contents = document.querySelectorAll('.contentOpen');
@@ -16,12 +16,11 @@ contents.forEach((content) => {
     content.style.visibility = "hidden";
 })
 
+
+// For the pros /////////////////////////////////////
+
 pro1.querySelector('.contentOpen').style.visibility = "visible";
 pro1.querySelector('.contentClosed').style.visibility = "hidden";
-
-app1.querySelector('.contentOpen').style.visibility = "visible";
-app1.querySelector('.contentClosed').style.visibility = "hidden";
-
 
 pros.forEach((pro) => {
     pro.addEventListener('click',()=>{
@@ -29,18 +28,34 @@ pros.forEach((pro) => {
         pros.forEach((pro) => {
             pro.classList.remove('active');
             pro.querySelector('.contentOpen').style.visibility = "hidden";
-            pro.querySelector('.contentClosed').style.visibility = "visible";    
+            pro.querySelector('.contentClosed').style.visibility = "visible";   
+            pro.style.flex = ''; 
         })
 
         pro.classList.add('active');
         pro.querySelector('.contentOpen').style.visibility = "visible";
         pro.querySelector('.contentClosed').style.visibility = "hidden";
+        pro.style.flex = '3';
     })
+
+    pro.addEventListener('mouseover', () => {
+        if(!pro.classList.contains('active')){
+            pro.style.flex = '1';
+        }
+    })
+    pro.addEventListener('mouseout', () => {
+        if(!pro.classList.contains('active')){
+            pro.style.flex = '';
+        }
+    })
+
 })
 
 
+// For the apps ////////////////////////////////////
 
-
+app1.querySelector('.contentOpen').style.visibility = "visible";
+app1.querySelector('.contentClosed').style.visibility = "hidden";
 
 apps.forEach((app) => {
     app.addEventListener('click',()=>{
@@ -48,14 +63,277 @@ apps.forEach((app) => {
         apps.forEach((app) => {
             app.classList.remove('active');
             app.querySelector('.contentOpen').style.visibility = "hidden";
-            app.querySelector('.contentClosed').style.visibility = "visible";    
+            app.querySelector('.contentClosed').style.visibility = "visible";  
+            app.style.flex = '';
         })
 
         app.classList.add('active');
         app.querySelector('.contentOpen').style.visibility = "visible";
         app.querySelector('.contentClosed').style.visibility = "hidden";
+        app.style.flex = '3';
     })
+
+    app.addEventListener('mouseover', () => {
+        if(!app.classList.contains('active')){
+            app.style.flex = '0.6';
+        }
+    })
+    app.addEventListener('mouseout', () => {
+        if(!app.classList.contains('active')){
+            app.style.flex = '';
+        }
+    })
+    
 })
+
+
+
+
+
+
+// Drag and drop ///////////////////////////////////
+
+
+const dragElements = document.querySelectorAll('.drag--q1');
+
+dragElements.forEach(function(el) {
+  let newPosX = 0, newPosY = 0, startPosX = 0, startPosY = 0;
+
+  const container = document.querySelector('.container--q1');
+  const containerRect = container.getBoundingClientRect();
+  const containerWidth = containerRect.width;
+  const containerHeight = containerRect.height;
+
+  const maxX = containerWidth - el.offsetWidth;
+  const maxY = containerHeight - el.offsetHeight;
+
+  el.addEventListener('mousedown', function(e){
+    e.preventDefault();
+    startPosX = e.clientX;
+    startPosY = e.clientY;
+    e.target.style.zIndex = parseInt(e.target.style.zIndex || 0) + 1;
+
+    document.addEventListener('mousemove', mouseMove);
+
+    document.addEventListener('mouseup', function(){
+      document.removeEventListener('mousemove', mouseMove);
+    });
+  });
+
+  function mouseMove(e) {
+    newPosX = startPosX - e.clientX;
+    newPosY = startPosY - e.clientY;
+
+    startPosX = e.clientX;
+    startPosY = e.clientY;
+
+    const newLeft = Math.max(0, Math.min(el.offsetLeft - newPosX, maxX));
+    const newTop = Math.max(0, Math.min(el.offsetTop - newPosY, maxY));
+
+    el.style.top = newTop + "px";
+    el.style.left = newLeft + "px";
+  }
+
+
+  el.addEventListener('click', function() {
+    // Get the maximum z-index among all elements
+    const maxZIndex = Array.from(document.querySelectorAll('.drag'))
+      .reduce((max, el) => Math.max(max, +el.style.zIndex || 0), 0);
+    
+    // Set the clicked element's z-index to be one greater than the maximum
+    el.style.zIndex = maxZIndex + 1;
+  });
+});
+
+
+
+const dragElementsQ2 = document.querySelectorAll('.drag--q2');
+
+dragElementsQ2.forEach(function(el) {
+    let newPosX = 0, newPosY = 0, startPosX = 0, startPosY = 0;
+  
+    const container = document.querySelector('.container--q2');
+    const containerRect = container.getBoundingClientRect();
+    const containerWidth = containerRect.width;
+    const containerHeight = containerRect.height;
+  
+    const maxX = containerWidth - el.offsetWidth;
+    const maxY = containerHeight - el.offsetHeight;
+  
+    el.addEventListener('mousedown', function(e){
+      e.preventDefault();
+      startPosX = e.clientX;
+      startPosY = e.clientY;
+  
+      document.addEventListener('mousemove', mouseMove);
+  
+      document.addEventListener('mouseup', function(){
+        document.removeEventListener('mousemove', mouseMove);
+      });
+    });
+  
+    function mouseMove(e) {
+      newPosX = startPosX - e.clientX;
+      newPosY = startPosY - e.clientY;
+  
+      startPosX = e.clientX;
+      startPosY = e.clientY;
+  
+      const newLeft = Math.max(0, Math.min(el.offsetLeft - newPosX, maxX));
+      const newTop = Math.max(0, Math.min(el.offsetTop - newPosY, maxY));
+  
+      el.style.top = newTop + "px";
+      el.style.left = newLeft + "px";
+    }
+
+    el.addEventListener('click', function() {
+        // Get the maximum z-index among all elements
+        const maxZIndex = Array.from(document.querySelectorAll('.drag'))
+          .reduce((max, el) => Math.max(max, +el.style.zIndex || 0), 0);
+        
+        // Set the clicked element's z-index to be one greater than the maximum
+        el.style.zIndex = maxZIndex + 1;
+      });
+  });
+  
+  
+
+
+const dragElementsQ3 = document.querySelectorAll('.drag--q3');
+
+dragElementsQ3.forEach(function(el) {
+    let newPosX = 0, newPosY = 0, startPosX = 0, startPosY = 0;
+  
+    const container = document.querySelector('.container--q3');
+    const containerRect = container.getBoundingClientRect();
+    const containerWidth = containerRect.width;
+    const containerHeight = containerRect.height;
+  
+    const maxX = containerWidth - el.offsetWidth;
+    const maxY = containerHeight - el.offsetHeight;
+  
+    el.addEventListener('mousedown', function(e){
+      e.preventDefault();
+      startPosX = e.clientX;
+      startPosY = e.clientY;
+  
+      document.addEventListener('mousemove', mouseMove);
+  
+      document.addEventListener('mouseup', function(){
+        document.removeEventListener('mousemove', mouseMove);
+      });
+    });
+  
+    function mouseMove(e) {
+      newPosX = startPosX - e.clientX;
+      newPosY = startPosY - e.clientY;
+  
+      startPosX = e.clientX;
+      startPosY = e.clientY;
+  
+      const newLeft = Math.max(0, Math.min(el.offsetLeft - newPosX, maxX));
+      const newTop = Math.max(0, Math.min(el.offsetTop - newPosY, maxY));
+  
+      el.style.top = newTop + "px";
+      el.style.left = newLeft + "px";
+    }
+  });
+
+  
+
+
+
+const dragElementsQ4 = document.querySelectorAll('.drag--q4');
+
+dragElementsQ4.forEach(function(el) {
+    let newPosX = 0, newPosY = 0, startPosX = 0, startPosY = 0;
+  
+    const container = document.querySelector('.container--q4');
+    const containerRect = container.getBoundingClientRect();
+    const containerWidth = containerRect.width;
+    const containerHeight = containerRect.height;
+  
+    const maxX = containerWidth - el.offsetWidth;
+    const maxY = containerHeight - el.offsetHeight;
+  
+    el.addEventListener('mousedown', function(e){
+      e.preventDefault();
+      startPosX = e.clientX;
+      startPosY = e.clientY;
+  
+      document.addEventListener('mousemove', mouseMove);
+  
+      document.addEventListener('mouseup', function(){
+        document.removeEventListener('mousemove', mouseMove);
+      });
+    });
+  
+    function mouseMove(e) {
+      newPosX = startPosX - e.clientX;
+      newPosY = startPosY - e.clientY;
+  
+      startPosX = e.clientX;
+      startPosY = e.clientY;
+  
+      const newLeft = Math.max(0, Math.min(el.offsetLeft - newPosX, maxX));
+      const newTop = Math.max(0, Math.min(el.offsetTop - newPosY, maxY));
+  
+      el.style.top = newTop + "px";
+      el.style.left = newLeft + "px";
+    }
+
+    el.addEventListener('click', function() {
+        const maxZIndex = Array.from(document.querySelectorAll('.drag'))
+          .reduce((max, el) => Math.max(max, +el.style.zIndex || 0), 0);
+        
+        el.style.zIndex = maxZIndex + 1;
+      });
+  });
+
+
+
+
+// Mobile ///////////////////////////////////////////////////////:::
+
+dragElements.forEach(function(el) {
+    let newPosX = 0, newPosY = 0, startPosX = 0, startPosY = 0;
+  
+    const container = document.querySelector('.container--q1');
+    const containerRect = container.getBoundingClientRect();
+    const containerWidth = containerRect.width;
+    const containerHeight = containerRect.height;
+  
+    const maxX = containerWidth - el.offsetWidth;
+    const maxY = containerHeight - el.offsetHeight;
+  
+    el.addEventListener('touchstart', function(e) {
+      e.preventDefault();
+      startPosX = e.touches[0].clientX;
+      startPosY = e.touches[0].clientY;
+  
+      document.addEventListener('touchmove', touchMove);
+  
+      document.addEventListener('touchend', function() {
+        document.removeEventListener('touchmove', touchMove);
+      });
+    });
+  
+    function touchMove(e) {
+      newPosX = startPosX - e.touches[0].clientX;
+      newPosY = startPosY - e.touches[0].clientY;
+  
+      startPosX = e.touches[0].clientX;
+      startPosY = e.touches[0].clientY;
+  
+      const newLeft = Math.max(0, Math.min(el.offsetLeft - newPosX, maxX));
+      const newTop = Math.max(0, Math.min(el.offsetTop - newPosY, maxY));
+  
+      el.style.top = newTop + "px";
+      el.style.left = newLeft + "px";
+    }
+  });
+  
+
 
 
 
@@ -136,7 +414,7 @@ if(prev){
 
 // Move with mouse ///////////////////////////////////
 
-let container = document.querySelector('.interviews');
+// let container = document.querySelector('.interviews');
 let objects = document.querySelectorAll('.object');
 
 document.addEventListener("mousemove", function move(e){
@@ -174,7 +452,8 @@ gsap.utils.toArray(".container__parallax").forEach((container, i) => {
 // }, {
 
 
-    backgroundPosition: `50% ${-window.innerHeight * getRatio(container)}px`
+    backgroundPosition: `50% ${-window.innerHeight * getRatio(container)}px`,
+    backgroundSize: "120%"
   }, {
     backgroundPosition: `50% ${window.innerHeight * (1 - getRatio(container))}px`,
     ease: "none",
@@ -183,8 +462,19 @@ gsap.utils.toArray(".container__parallax").forEach((container, i) => {
       start: "top bottom",
       end: "bottom top",
       scrub: true,
-      invalidateOnRefresh: true // to make it responsive
+      invalidateOnRefresh: true, 
+
+      onUpdate: (self) => {
+        const scrollSpeed = 14; 
+        const newBackgroundPosition = `50% ${-window.scrollY / scrollSpeed}px`;
+        gsap.set(container.bgp, { backgroundPosition: newBackgroundPosition });
+      }
     }
   });
 
 });
+
+
+
+
+
